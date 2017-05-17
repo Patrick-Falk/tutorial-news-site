15feb67c51d49178848b90faa4ca6415acb9cf14
Patrick-Falk <pakfalk@gmail.com>
2017-05-17 13:49:04 -0500

    Added the create functionality and a form to the system

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -37,4 +37,29 @@ class News extends CI_Controller{
         $this->load->view('news/view', $data);
         $this->load->view('templates/footer');
     }
+
+    public function create()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $data['title'] = 'Create a news item';
+
+//      Parameters: name of input field, name used in error messages, and the rule
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'Text', 'required');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('news/create');
+            $this->load->view('templates/footer');
+
+        }
+        else
+        {
+            $this->news_model->set_news();
+            $this->load->view('news/success');
+        }
+    }
 }
